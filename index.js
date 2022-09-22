9fc59fae5811147bf50b3e7db7a6a0ee2a74d90b
Hrqlv <henriquelopesvelozo@gmail.com>
2022-09-22 20:02:33 -0300

    add edit music

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const Music = require("./models/Music");
 const app = express();
 const port = process.env.PORT || 3000;
 
+//variável p usar no editar musica
+let music = null;
 //motor q mostrara as telas para cliente é 'ejs
 app.set("view engine", "ejs");
 
@@ -31,9 +33,13 @@ app.get("/", async (req, res) => {
 });
 
 //rota p enviar usuário para admin
-app.get("/admin", (req, res) => {
-  res.render("admin");
+app.get("/admin", async (req, res) => {
+  const playList = await Music.find();
+
+  //renderizar playList e music (music sempre null qd recarregar pagina)
+  res.render("admin", { playList, music: null });
 });
+
 
 //rota de cadastro
 app.post("/create", async (req, res) => {
@@ -44,6 +50,43 @@ app.post("/create", async (req, res) => {
   await Music.create(music);
   res.redirect("/");
 });
+
+
+//rota editar musica
+//by primeiro endereço
+//id para receber pror parametro na rota um item aleatório
+app.get("/by/:id", async (req, res) => {
+
+  //peguei o id do parâmetros
+  const { id } = req.params;
+
+  //variavel criada acima como null 
+  // pesquisar id no banco de dados e colocar n variavel 'music
+  music = await Music.findById({_id: id})
+
+  //coloco aki, dou find all e renderizo novamente
+  //passando playlist e music
+  const playList = await Music.find();
+  //renderizar playList e music
+  res.render("admin", { playList, music });
+});
+
+
+//rota update musica
+app.post('/update/:id', async (req, res)=>{
+    //trazer do body
+    const newMusic = req.body
+
+    //update no id q vier de req.params
+    //e editar colocando a nova musica 'newMusic'
+    await Music.updateOne({_id: req.params.id}, newMusic)
+
+
+    
+    // redirecionar para admin
+    // e depois fecha o modal como "null"
+    res.redirect('/admin')
+})
 app.listen(port, () =>
   console.log(`servidor executado em http://localhost:${port}`)
 );
